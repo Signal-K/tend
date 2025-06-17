2ac571d390e4f280a35df09da9afef27519866b2
Gizmotronn <liam@skinetics.tech>
2025-06-17 16:38:11 -0500

    Initial Commit

---
+++ Tend/TendApp.swift
@@ -0,0 +1,17 @@
+//
+//  TendApp.swift
+//  Tend
+//
+//  Created by Liam Arbuckle on 17/6/2025.
+//
+
+import SwiftUI
+
+@main
+struct TendApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
